36aa36c80b97fde996c8247e939e4ffa8b4957cf
id4thomas <id2thomas@gmail.com>
2024-07-13 01:13:49 +0900

    [add] obstacle generator inference

--- 1_conflict_generation/obstacle_generator/src/obstacle_generator.py
+++ 1_conflict_generation/obstacle_generator/src/obstacle_generator.py
@@ -7,12 +7,15 @@ from transformers import (
 	TextGenerationPipeline
 )
 
+from nltk.tokenize import sent_tokenize
+
 class ObstacleGenerator(object):
 	def __init__(
 		self,
 		model: PreTrainedModel,
 		tokenizer: PreTrainedTokenizerBase,
-		device: torch.device = torch.device('cuda')
+		device: torch.device = torch.device('cuda'),
+		mode = "phu"
 	):
 		model.eval()
 		# TextGenerationPipeline
@@ -21,9 +24,10 @@ class ObstacleGenerator(object):
 			tokenizer = tokenizer,
 			device = device
 		)
+		self.mode = mode
 
 	def _make_defeasible_gen_input(self, context: str, goal: str):
-		return f"[premise] {context} [hypo] {goal} [weakener]"
+		return f"<|startoftext|>[premise] {context} [hypo] {goal} [weakener]"
 	
 	def _generate_text(self, text: List[str], decode_params: dict):
 		with torch.no_grad():
@@ -36,18 +40,28 @@ class ObstacleGenerator(object):
 		generated_text: List[List[str]] = []
 		for text_generated in generated:
 			if isinstance(text_generated, list):
-				text_generated_text = [x["generated_text"] for x in text_generated]
+				text_generated_text = [x["generated_text"].strip() for x in text_generated]
 			else:
-				text_generated_text = [text_generated["generated_text"]]
+				text_generated_text = [text_generated["generated_text"].strip()]
 				pass
 			generated_text.append(text_generated_text)
 		return generated_text
 	
+	def _get_first_sentence(self, text: str):
+		sentences = sent_tokenize(text)
+		first_sentence = sentences[0].strip()
+		return first_sentence
 	
 	def _generate_obstacles(self, contexts: List[str], goals: List[str], decode_params: dict) -> List[List[str]]:
 		input_texts = [self._make_defeasible_gen_input(c, g) for c,g in zip(contexts, goals)]
 		generated_texts = self._generate_text(input_texts, decode_params)
-		return generated_texts
+
+		## only get first sentences
+		obstacles = []
+		for sample_generated_texts in generated_texts:
+			sample_obstacles = list(map(self._get_first_sentence, sample_generated_texts))
+			obstacles.append(sample_obstacles)
+		return obstacles
 		
 	def generate(
 		self,
